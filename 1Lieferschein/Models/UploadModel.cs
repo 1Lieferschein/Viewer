6030245bf0e0f68fc7d06e8dc46bfd684e1c5430
Jessica Schneider <j.schneider@leonhard-weiss.com>
2021-11-16 17:14:16 +0100

    Xml readable

---
+++ 1Lieferschein/Models/UploadModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace _1Lieferschein.Models
+{
+    public class UploadModel
+    {
+        public string FilePath { get; set; }
+
+        public IFormFile UploadFile { get; set; }
+    }
+}
